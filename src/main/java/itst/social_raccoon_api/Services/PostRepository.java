bd39d7daab1b65fe6a6bf2afb0bf02124f455e3b
MarcoAntonioHernandez <lm682127@gmail.com>
2024-10-11 10:11:10 -0600

    Create PostRepository.java

---
+++ src/main/java/itst/social_raccoon_api/Services/PostRepository.java
@@ -0,0 +1,10 @@
+package itst.social_raccoon_api.Repositories;
+
+import itst.social_raccoon_api.Models.PostModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<PostModel, Integer> {
+
+}

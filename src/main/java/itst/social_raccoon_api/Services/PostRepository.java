1f9f9f7f18daa94490ddfd8c6227a816a1df7fe0
MarcoAntonioHernandez <lm682127@gmail.com>
2024-10-11 10:06:38 -0600

    Update PostRepository.java

--- src/main/java/itst/social_raccoon_api/Services/PostRepository.java
+++ src/main/java/itst/social_raccoon_api/Services/PostRepository.java
@@ -1,11 +1,44 @@
-ackage itst.social_raccoon_api.Repositories;
+package itst.social_raccoon_api.Services;
 
 import itst.social_raccoon_api.Models.PostModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import itst.social_raccoon_api.Repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-@Repository
-public interface PostRepository extends JpaRepository<PostModel, Integer> {
-    
-    
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public List<PostModel> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public Optional<PostModel> getPostById(Integer id) {
+        return postRepository.findById(id);
+    }
+
+    public PostModel savePost(PostModel post) {
+        return postRepository.save(post);
+    }
+
+    public void deletePost(Integer id) {
+        postRepository.deleteById(id);
+    }
+
+    public PostModel updatePost(Integer id, PostModel postDetails) {
+        Optional<PostModel> post = postRepository.findById(id);
+        if (post.isPresent()) {
+            PostModel existingPost = post.get();
+            existingPost.setDescription(postDetails.getDescription());
+            existingPost.setImageUrl(postDetails.getImageUrl());
+            existingPost.setDateCreated(postDetails.getDateCreated());
+            return postRepository.save(existingPost);
+        }
+        return null;
+    }
 }

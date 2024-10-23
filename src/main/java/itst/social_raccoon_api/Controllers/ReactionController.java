2f3991672a56d301124b1f33cbfe081bd1bee44c
Gera1UP <monterovallejogerardo@gmail.com>
2024-10-22 22:38:04 -0600

    Agregado de paginación a Reaction

--- src/main/java/itst/social_raccoon_api/Controllers/ReactionController.java
+++ src/main/java/itst/social_raccoon_api/Controllers/ReactionController.java
@@ -5,14 +5,11 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import itst.social_raccoon_api.Dto.ReactionDTO;
-import itst.social_raccoon_api.Services.PostService;
-import itst.social_raccoon_api.Services.ReactionTypeService;
-import itst.social_raccoon_api.Services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -22,12 +19,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import itst.social_raccoon_api.Dto.ReactionDTO;
 import itst.social_raccoon_api.Models.ReactionModel;
+import itst.social_raccoon_api.Services.PostService;
 import itst.social_raccoon_api.Services.ReactionService;
+import itst.social_raccoon_api.Services.ReactionTypeService;
+import itst.social_raccoon_api.Services.UserService;
+
 
 @RestController
 @RequestMapping("reactions")
@@ -50,26 +55,38 @@ public class ReactionController {
     @Autowired
     private ReactionTypeService reactionTypeService;
 
-    @Operation(summary = "Get all reactions")
+    @Operation(summary = "Get all reactions with pagination")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Found reactions"),
-            @ApiResponse(responseCode = "404", description = "Reactions not found")
+        @ApiResponse(responseCode = "200", description = "Found reactions"),
+        @ApiResponse(responseCode = "404", description = "Reactions not found")
     })
     @GetMapping
-    public List<ReactionDTO> getAll() {
-        List<ReactionModel> reactions = reactionService.getAll();
-        if (reactions.isEmpty()) {
+    public ResponseEntity<List<ReactionDTO>> getAll(
+            @RequestParam(defaultValue = "0") int page, 
+            @RequestParam(defaultValue = "10") int size) {
+        
+        Pageable pageable = PageRequest.of(page, size);
+        Page<ReactionModel> reactionPage = reactionService.getAll(pageable);
+    
+        if (reactionPage.isEmpty()) {
             throw new NoSuchElementException();
         }
-        return reactions.stream()
+    
+        // Extract only the content of the page
+        List<ReactionDTO> reactionDTOList = reactionPage
+                .getContent()
+                .stream()
                 .map(this::convertToDTO)
                 .collect(Collectors.toList());
+    
+        return new ResponseEntity<>(reactionDTOList, HttpStatus.OK);
     }
+    
 
     @Operation(summary = "Get a reaction by its post id and user id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Found reaction"),
-            @ApiResponse(responseCode = "404", description = "Reaction not found")
+        @ApiResponse(responseCode = "200", description = "Found reaction"),
+        @ApiResponse(responseCode = "404", description = "Reaction not found")
     })
     @GetMapping("/post/{postId}/user/{userId}")
     public ResponseEntity<ReactionDTO> getById(@PathVariable Integer postId, @PathVariable Integer userId) {
@@ -82,8 +99,8 @@ public class ReactionController {
 
     @Operation(summary = "Get all reactions by post id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Found reactions"),
-            @ApiResponse(responseCode = "404", description = "Reactions not found")
+        @ApiResponse(responseCode = "200", description = "Found reactions"),
+        @ApiResponse(responseCode = "404", description = "Reactions not found")
     })
     @GetMapping("/post/{postId}")
     public List<ReactionDTO> getReactionsByPostId(@PathVariable Integer postId) {
@@ -98,8 +115,8 @@ public class ReactionController {
 
     @Operation(summary = "Get all reactions by user id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Found reactions"),
-            @ApiResponse(responseCode = "404", description = "Reactions not found")
+        @ApiResponse(responseCode = "200", description = "Found reactions"),
+        @ApiResponse(responseCode = "404", description = "Reactions not found")
     })
     @GetMapping("/user/{userId}")
     public List<ReactionDTO> getReactionsByUserId(@PathVariable Integer userId) {
@@ -112,8 +129,8 @@ public class ReactionController {
 
     @Operation(summary = "Get reaction count by post id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Reaction count retrieved"),
-            @ApiResponse(responseCode = "404", description = "Post not found")
+        @ApiResponse(responseCode = "200", description = "Reaction count retrieved"),
+        @ApiResponse(responseCode = "404", description = "Post not found")
     })
     @GetMapping("/count/post/{postId}")
     public Integer getReactionCountByPostId(@PathVariable Integer postId) {
@@ -125,8 +142,8 @@ public class ReactionController {
 
     @Operation(summary = "Get reaction count by post id and reaction type id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Reaction count retrieved"),
-            @ApiResponse(responseCode = "404", description = "Post or reaction type not found")
+        @ApiResponse(responseCode = "200", description = "Reaction count retrieved"),
+        @ApiResponse(responseCode = "404", description = "Post or reaction type not found")
     })
     @GetMapping("/count/post/{postId}/reactionType/{reactionTypeId}")
     public Integer getReactionCountByPostIdAndReactionTypeId(@PathVariable Integer postId, @PathVariable Integer reactionTypeId) {
@@ -138,8 +155,8 @@ public class ReactionController {
 
     @Operation(summary = "React to a post or update an existing reaction.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Reaction updated"),
-            @ApiResponse(responseCode = "404", description = "Post or user not found")
+        @ApiResponse(responseCode = "200", description = "Reaction updated"),
+        @ApiResponse(responseCode = "404", description = "Post or user not found")
     })
     @PostMapping("/post/{postId}/user/{userId}/reaction/{reactionTypeId}")
     public ResponseEntity<ReactionDTO> reactOrUpdate(@PathVariable Integer postId, @PathVariable Integer userId, @PathVariable Integer reactionTypeId) {
@@ -149,8 +166,8 @@ public class ReactionController {
 
     @Operation(summary = "Delete a user's reaction to a post.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Reaction deleted"),
-            @ApiResponse(responseCode = "404", description = "Reaction not found")
+        @ApiResponse(responseCode = "200", description = "Reaction deleted"),
+        @ApiResponse(responseCode = "404", description = "Reaction not found")
     })
     @DeleteMapping("/post/{postId}/user/{userId}")
     public ResponseEntity<Map<String, Boolean>> deleteReaction(@PathVariable Integer postId, @PathVariable Integer userId) {
